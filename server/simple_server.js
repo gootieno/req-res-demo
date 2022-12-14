bafc00aa1efea6b7ab9bd4bf4a52d2a6e857d828
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-12-14 15:58:05 -0800

    client + server working

--- server/simple_server.js
+++ server/simple_server.js
@@ -1,9 +1,9 @@
-const http = require('http')
-const fs = require('fs')
-const serverUtils = require('./server_utils')
+const http = require("http");
+const fs = require("fs");
+const serverUtils = require("./server_utils");
 
-const commentsArray = []
-const path = ''
+const commentsArray = [];
+const path = "../client/index.html";
 
 const server = http.createServer((req, res) => {
   let reqBody = "";
@@ -11,9 +11,29 @@ const server = http.createServer((req, res) => {
     reqBody += data;
   });
 
+  if (req.method === "GET" && req.url.startsWith("/static")) {
+    const urlParts = req.url.split("/static");
 
-})
+    const staticPath = urlParts[1];
 
-const port = 5000
+    const resBody = fs.readFileSync("./" + staticPath);
 
-server.listen(port, () => console.log(`Success: listening on port ${port}...`))
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/css");
+    return res.end(resBody);
+  }
+
+  if (req.method === "GET" && req.url === "/") {
+    const htmlPage = fs.readFileSync(path, "utf-8");
+
+    let resBody = htmlPage.replace("#{comments}", commentsArray);
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    return res.end(resBody);
+  }
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log(`Success: listening on port ${port}...`));
