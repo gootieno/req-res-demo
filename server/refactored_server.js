b8dd35ae3afa61aa405c133c61ae6fb5a6bfc9ee
Drewthurm21 <drewthurm21@gmail.com>
2022-12-14 21:02:51 -0600

    added added to server utils and wrote refactored server file for reference

---
+++ server/refactored_server.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const {
+  serverRouter,
+  serverUtils: { parseReqBody, parseFilePath }
+} = require("./server_utils");
+
+
+const server = http.createServer((req, res) => {
+  let reqBody = "";
+  req.on("data", (data) => {
+    reqBody += data;
+  });
+
+  req.on("end", () => {
+    // Parsing the body of the request
+    if (reqBody) {
+      req.body = parseReqBody(reqBody)
+    }
+
+    if (req.method === "GET" && req.url.startsWith("/static")) {
+      const assetPath = parseFilePath(req)
+      return serverRouter.serveAsset(req, res, assetPath)
+    }
+
+    if (req.method === "GET" && req.url === "/") {
+      return serverRouter.GET.home(req, res)
+    }
+
+    if (req.method === "POST" && req.url === "/comments") {
+      return serverRouter.POST.comments(req, res)
+    }
+  });
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log(`Success: listening on port ${port}...`));
--- server/server_utils.js
+++ server/server_utils.js
@@ -1,8 +1,21 @@
 const fs = require('fs')
+const homePagePath = "../client/index.html";
+
+/*
+in some cases you will find a server utils file, or folder
+and I want you to have some exposure to what that may look like
+
+some of the advantages of having a utilities file are:
+1.you can group relevant / related bits of logic together
+2.it will help you follow the single responsibility principle
+3.it will help keep your code DRY
+*/
+
+let commentsArray = []
 
 const serverUtils = {
   parseReqBody: (encodedString) => {
-    encodedString
+    return encodedString
       .split("&").map((keyValuePair) => keyValuePair.split("="))
       .map(([key, value]) => [key, value.replace(/\+/g, " ")])
       .map(([key, value]) => [key, decodeURIComponent(value)])
@@ -12,18 +25,79 @@ const serverUtils = {
       }, {});
   },
 
-  injectHtmlData: (html, tag, data) => {
-    const regex = new RegExp(tag, "g")
-    html = html.replace(regex, data)
+  injectHtmlData: (html, tags, data) => {
+    for (const tag of tags) {
+      const regex = new RegExp(tag, "g")
+      html = html.replace(regex, data)
+    }
     return injectedHtml
   },
 
   getFileText: (path) => {
-    const fileText = fs.readFileSync(path, 'utf-8');
-    return fileText
+    return fs.readFileSync(path, 'utf-8');
+  },
+
+  parseFilePath: (req) => {
+    return '.' + req.url.split("/static")[1]
+  },
+
+  setContentType: (fileType) => {
+    switch (fileType) {
+      case "jpg":
+      case "jpeg":
+        return "image/jpeg";
+      case "png":
+        return "image/png";
+      case "css":
+        return "text/css";
+      case "js":
+        return "text/javascript";
+      default:
+        return "text/plain";
+    }
+  }
+}
+
+let serverRouter = {
+  /* --- HANDLE STATIC ASSET --- */
+  'serveAsset': (req, res, assetPath) => {
+    const fileType = assetPath.split('/')[1]
+    const resBody = serverUtils.getFileText(assetPath)
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', serverUtils.setContentType(fileType));
+    return res.end(resBody);
   },
 
 
+  /* --- GET ROUTES --- */
+  'GET': {
+    'home': (req, res) => {
+      const htmlPage = serverUtils.getFileText(homePagePath);
+      const commentsList = commentsArray.map(comment => `<li>${comment}</li>`);
+      const resBody = htmlPage.replace(/#{comments}/g, commentsList.join(""));
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      return res.end(resBody);
+    },
+  },
+
+
+  /* --- POST ROUTES --- */
+  'POST': {
+    'comments': (req, res) => {
+      commentsArray.push(req.body.comment);
+
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    }
+  }
 }
 
-module.exports = serverUtils
+module.exports = {
+  commentsArray,
+  serverUtils,
+  serverRouter,
+}
